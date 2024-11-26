8ebdfaae10aea361f3f745e5b45532abb75a9f97
Franklin Ronald <franklin@wiselabs.com.br>
2024-11-26 02:09:42 -0300

    Lint

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
-rm -Rf ./build ./.flatpak-builder
-rm -f generated-sources.json
-flatpak-node-generator yarn -r ../simplexity/yarn.lock
+# rm -Rf ./build ./.flatpak-builder
 flatpak-builder build -v br.com.wiselabs.simplexity.yml --ccache --install-deps-from=flathub --force-clean --user --install
 
---
+++ lint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest br.com.wiselabs.simplexity.yml
+
